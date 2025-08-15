241bc11ac4c08f002599f43a3f3ac75f32a8a88a
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2025-08-15 21:38:16 +0300

    feat: add two layout components

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,5 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-    "*.{js, jsx, ts, tsx, css, json, html}": "prettier --write",
+    "*.{js,jsx,ts,tsx,css,json,html}": "prettier --write",
 };
