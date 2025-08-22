32fde07197e7de1d35ce954094a79d0590e850f3
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2025-08-22 16:45:32 +0300

    feat: create board card component

---
+++ src/features/boards/components/BoardCard.tsx
@@ -0,0 +1,64 @@
+import { type FC } from "react";
+import { BoardType, type BoardDto } from "../dtos/board.dto";
+import { Link } from "react-router-dom";
+import DefaultImg1 from "../../../assets/images/board-1.svg";
+import DefaultImg2 from "../../../assets/images/board-2.svg";
+import DefaultImg3 from "../../../assets/images/board-3.svg";
+import DefaultImg4 from "../../../assets/images/board-4.svg";
+import DefaultImg5 from "../../../assets/images/board-5.svg";
+import DefaultImg6 from "../../../assets/images/board-6.svg";
+import DefaultImg7 from "../../../assets/images/board-7.svg";
+import clsx from "clsx";
+
+/**
+ * Every card picks an image based on the Id of the card.
+ * Note that this only valid in case of the Id is
+ * an integer.
+ */
+const CardBuiltInImages: string[] = [
+    DefaultImg1,
+    DefaultImg2,
+    DefaultImg3,
+    DefaultImg4,
+    DefaultImg5,
+    DefaultImg6,
+    DefaultImg7,
+];
+
+const BoardCard: FC<BoardDto> = (board) => {
+    const badgeClasses = clsx(
+        "text-xs border-1 px-3 py-0.5 rounded-full leading-[1.5]",
+        {
+            "border-success text-success bg-success/10 ":
+                board.type === BoardType.PUBLIC,
+            "border-secondary text-primary bg-secondary/10":
+                board.type === BoardType.PRIVATE,
+        },
+    );
+
+    return (
+        <Link
+            to={`/app/boards/${board.id}`}
+            className="border-1 border-primary-border rounded-lg flex flex-col h-[325px] p-1 shadow-xs"
+        >
+            <img
+                src={CardBuiltInImages[board.id % CardBuiltInImages.length]}
+                alt="board image"
+                className="h-2/3 rounded-md bg-primary/7.5 p-2"
+            />
+            <div className="flex flex-col px-2 py-6 h-1/3">
+                <div className="flex justify-between">
+                    <h1 className="text-base">{board.title}</h1>
+                    <span className={badgeClasses}>{board.type}</span>
+                </div>
+                <div className="mt-auto">
+                    <span className="text-xs opacity-50">
+                        Last modified at {board.updated_at}
+                    </span>
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default BoardCard;
--- src/features/boards/components/BoardsGrid.tsx
+++ src/features/boards/components/BoardsGrid.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import CustomButton from "../../../components/CustomButton";
 import { HiPlus } from "react-icons/hi2";
 import CreateBoardModal from "./CreateBoardModal";
+import { BoardType, type BoardDto } from "../dtos/board.dto";
+import BoardCard from "./BoardCard";
 
 const BoardsGrid = () => {
     const [createBoardModalOpen, setCreateBoardModalOpen] = useState(false);
@@ -10,8 +12,36 @@ const BoardsGrid = () => {
         setCreateBoardModalOpen((prev) => nextState ?? !prev);
     };
 
+    const boards: BoardDto[] = [
+        {
+            id: 1,
+            title: "Board 1.2",
+            description: "",
+            type: BoardType.PUBLIC,
+            created_at: new Date().toLocaleDateString(),
+            updated_at: new Date().toLocaleDateString(),
+        },
+        {
+            id: 2,
+            title: "Board 1.3",
+            description: "",
+            type: BoardType.PRIVATE,
+            created_at: new Date().toLocaleDateString(),
+            updated_at: new Date().toLocaleDateString(),
+        },
+        {
+            id: 3,
+            title: "Board 1.4",
+            description: "",
+            type: BoardType.PRIVATE,
+            created_at: new Date().toLocaleDateString(),
+            updated_at: new Date().toLocaleDateString(),
+        },
+    ];
+
     return (
         <>
+            {/* Modals */}
             <CreateBoardModal
                 isOpen={createBoardModalOpen}
                 toggleOpen={toggleCreateBoardModalOpen}
@@ -25,6 +55,9 @@ const BoardsGrid = () => {
                     <HiPlus size={36} />
                     <span className="text-xl font-normal">New board</span>
                 </CustomButton>
+                {boards.map((board) => (
+                    <BoardCard key={board.id} {...board} />
+                ))}
             </div>
         </>
     );
