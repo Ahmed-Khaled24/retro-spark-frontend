aed48837cc789486ce0a3525f3eb7eef748100fc
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2025-09-04 15:25:35 +0300

    refactor: remove unused code

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,10 +1,9 @@
-import { Outlet, ScrollRestoration } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { PublicNavbar } from "./Nav";
 
 const AuthLayout = () => {
     return (
         <main className="flex flex-col min-h-screen px-8 bg-background">
-            <ScrollRestoration />
             <PublicNavbar />
             <div className="flex-1">
                 <Outlet />
