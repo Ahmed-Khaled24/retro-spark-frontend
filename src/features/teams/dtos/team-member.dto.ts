8f7149b7002983aa5b5aa94f0abed32981b31721
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2025-09-20 17:01:48 +0300

    feat: update team members roles

--- src/features/teams/dtos/team-member.dto.ts
+++ src/features/teams/dtos/team-member.dto.ts
@@ -2,6 +2,7 @@ import type { UserDto } from "../../auth/dtos/user.dto";
 
 export enum TeamMemberRole {
     OWNER = "OWNER",
+    ADMIN = "ADMIN",
     PARTICIPANT = "PARTICIPANT",
     FACILITATOR = "FACILITATOR",
 }
