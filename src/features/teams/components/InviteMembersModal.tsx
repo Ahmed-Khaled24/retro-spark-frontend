599e88877ab65671aee35cd1232a214e2d4bbad9
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2025-09-20 17:00:23 +0300

    refactor: enhance invite members modal

--- src/features/teams/components/InviteMembersModal.tsx
+++ src/features/teams/components/InviteMembersModal.tsx
@@ -7,42 +7,49 @@ import { MdContentCopy } from "react-icons/md";
 import { errorToast, successToast } from "../../../utils/toasters";
 import z from "zod";
 import { useInviteMemberMutation } from "../../invitations/InvitationsApi";
+import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import type { APIError } from "../../../app/api";
 
-interface AddMemberModalProps
+interface InviteMemberModalProps
     extends Pick<ModalProps, "isOpen" | "toggleOpen"> {}
 
-const AddMemberModal: FC<AddMemberModalProps> = (props) => {
+const InviteMembersModal: FC<InviteMemberModalProps> = (props) => {
     const { id } = useParams();
     const [memberEmails, setMemberEmails] = useState("");
     const [invite, { isLoading }] = useInviteMemberMutation();
 
     const invitationLink = `${import.meta.env.VITE_BASE_URL}/teams/invite?team=${id}`;
 
-    const inviteMembers = async () => {
-        const { data: emails, success } = await z.safeParseAsync(
-            z.array(z.email("not a valid email")),
-            memberEmails.split(","),
-        );
+    const inviteMembers = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-        if (!success) {
-            errorToast(
-                "Invalid email detected.\nMake sure all emails are valid.",
-            );
-            return;
+        // Validate emails one by one
+        let emails = memberEmails.split(",").map((email) => email.trim());
+        for (let email of emails) {
+            const { success } = await z.safeParseAsync(z.email(), email);
+            if (!success) {
+                errorToast(`${email} is not a valid email.`);
+                return;
+            }
         }
 
         await invite({
             emails: emails.join(","),
             teamId: parseInt(id!),
-        }).unwrap();
-
-        setMemberEmails("");
-        props.toggleOpen(false);
+        })
+            .unwrap()
+            .then(() => {
+                setMemberEmails("");
+                props.toggleOpen(false);
+            })
+            .catch((e: FetchBaseQueryError) =>
+                errorToast((e.data as APIError).message),
+            );
     };
 
     return (
-        <Modal title="Add team member" {...props}>
-            <div className="p-6 flex flex-col gap-6">
+        <Modal title="Invite members" {...props}>
+            <form className="p-6 flex flex-col gap-6" onSubmit={inviteMembers}>
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
                         <p>
@@ -70,6 +77,7 @@ const AddMemberModal: FC<AddMemberModalProps> = (props) => {
                         </span>
                     </div>
                     <CustomTextarea
+                        required
                         label="Add email addresses separated by a comma"
                         onChange={(e) => setMemberEmails(e.target.value)}
                         labelPosition="embedded"
@@ -81,24 +89,24 @@ const AddMemberModal: FC<AddMemberModalProps> = (props) => {
                 <div className="flex gap-2 flex-row-reverse">
                     <CustomButton
                         variant="secondary"
-                        className="rounded-lg"
-                        onClick={inviteMembers}
+                        className="rounded-lg w-30"
                         loading={isLoading}
+                        buttonType="submit"
                     >
                         Invite
                     </CustomButton>
                     <CustomButton
                         onClick={() => props.toggleOpen(false)}
-                        className="rounded-lg"
+                        className="rounded-lg w-30"
                         variant="secondary"
                         outlined
                     >
                         Cancel
                     </CustomButton>
                 </div>
-            </div>
+            </form>
         </Modal>
     );
 };
 
-export default AddMemberModal;
+export default InviteMembersModal;
